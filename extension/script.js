bb38946b17cc40f43f7405a1a1fd0a9f135b8f75
jason2020 <jtay20@punahou.edu>
2017-09-05 23:13:57 -1000

    Turning raw class data to JSON
    Worked on editing the raw class data extracted from the extension. Finished processing the key data for the classes (class name, teacher, time). Need to finish formatting the letter day's (A, B, C, etc.) data to be more easily used.

--- extension/script.js
+++ extension/script.js
@@ -1,32 +1,103 @@
 var tableData = $(".studentNoPaddingBorder2"),
 	timedata = $(".attendTypeColumnData2"),
 	i = 0,
-	j = 0,
+	k = 0,
+	m = 0,
 	tablelength = tableData.length,
 	timelength = timedata.length,
 	array = [[]],
+	masterData = {
+		schedule : {
+			"A-Day": [],
+			"B-Day": [],
+			"C-Day": [],
+			"D-Day": [],
+			"E-Day": [],
+			"F-Day": []
+		},
+		key: {}
+	},
 	currIndex = 0;
 
+// Pull class data from table
 for (i; i < tablelength; i++) {
 	if ((i + 1) % 5 === 0) {
 		array.push([])
 		array[currIndex].push($(tableData[i]).html());
 		currIndex = (i + 1) / 5;
 	} else {
-		console.log(array, currIndex);
 		array[currIndex].push($(tableData[i]).html());
 	}
 }
 
+// Pull class time from table
 currIndex = 0;
-for (j; j < timelength; j++) {
-	if ((j + 1) % 7 === 0) {
-		array[currIndex].push($(timedata[j]).html());
-		currIndex = (j + 1) / 7
+i = 0 
+for (i; i < timelength; i++) {
+	if ((i + 1) % 7 === 0) {
+		array[currIndex].push($(timedata[i]).html());
+		currIndex = (i + 1) / 7
 	} else {
-		console.log(array, currIndex);
-		array[currIndex].push($(timedata[j]).html());
+		array[currIndex].push($(timedata[i]).html());
 	}
 }
 
-// Roget, add the regex stuff here:
+console.log(array);
+
+// Process raw data into JSON data to be used in schdule maker
+for (k; k < array.length; k++) {
+	if (array[k].length > 0) {
+		var letterDay;
+		var l = 5;
+		masterData.key[k] = {};
+		masterData.key[k].className = array[k][0];
+		masterData.key[k].classCode = array[k][1];
+		masterData.key[k].teacher = array[k][2].match("\<\/a\>\&nbsp;(.+)")[1];
+		if (array[k][3] == "&nbsp;") {
+			masterData.key[k].classroom = "";
+		} else {
+			masterData.key[k].classroom = array[k][3];
+		}
+		masterData.key[k].semester = array[k][4];
+		for (l; l < array[k].length - 1; l++) {
+			switch (l) {
+				case 5:
+					letterDay = "A-Day";
+					break;
+				case 6:
+					letterDay = "B-Day";
+					break;
+				case 7:
+					letterDay = "C-Day";
+					break;
+				case 8:
+					letterDay = "D-Day";
+					break;
+				case 9:
+					letterDay = "E-Day";
+					break;
+				case 10:
+					letterDay = "F-Day";
+					break;
+			}
+			if (array[k][l] == "&nbsp;") {
+				// masterData.schedule[letterDay].push(".");
+			} else {
+				var classData = array[k][l].split("-");
+				if (classData.length < 2) {
+					classData[0] = parseInt(classData[0]);
+				} else {
+					classData[0] = parseInt(classData[0]);
+					classData[1] = parseInt(classData[1]);
+				}
+				classData.unshift(k);
+				masterData.schedule[letterDay].push(classData);
+			}
+		}	
+	} else {
+		console.log("empty array");
+	}
+}
+
+console.log(masterData);
+
