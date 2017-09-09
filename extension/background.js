3a743321eb7b9aa903eebf9dbd44e1073fbb2106
dizae <epiphanydiza@gmail.com>
2017-09-08 21:15:12 -1000

    Updates to Extension
    Updated manifest and popup.html to give permissions to appropriate url, created background.js to initiate browser action of creating new tab

---
+++ extension/background.js
@@ -0,0 +1,17 @@
+/*! The background javascript for the extension
+// here we use onClicked which will NOT work simultaneously with a default_popup in manifest.
+// Fix conflicts and add if else logic to determine whether user is logged in (Check elements)
+// If logged in > automatically open/close window in background and get student schedule info
+// If not logged in > change to activetab/redirect to page and prompt user login, then get schedule info
+*/
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.windows.create({ 
+        url: "https://mybackpack.punahou.edu/SeniorApps/studentParent/schedule.faces?selectedMenuId=true",
+        width:  430,
+        height: 150,
+        top:    top_popup,
+        left:   left_popup
+    }, function(win) {
+        chrome.windows.update(tab.windowId, { focused: true });
+    });
+});
