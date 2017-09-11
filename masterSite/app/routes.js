7947197fc692052c048045398a6944a21c0d8ea8
Brandon Yee <brandon.yee150@gmail.com>
2017-09-10 14:25:37 -1000

    Restarted Vue Project
    The original Vue project was also overloaded with unnecessary production tools. We also removed old projects to consolidate it into one project.

--- masterSite/app/routes.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var path = require("path");
-var router = express.Router();
-module.exports = router;
-
-router.get("/", function (req, res) {
-	// res.sendFile(path.join(__dirname, "../public/index.html"));
-});
