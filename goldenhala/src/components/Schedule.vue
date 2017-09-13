d3ccc4e3c0a099b578a43fad95f8ec2813a1d568
jason2020 <jtay20@punahou.edu>
2017-09-12 22:12:58 -1000

    Getting JSON --> schedule
    Using the amazing power of lodash, managed to get the json data into arrays by letter dat with class data and starting time. Going to turn data into GUI soon. Also worked on potentially finding a good library for us to use for schedule making.

--- goldenhala/src/components/Schedule.vue
+++ goldenhala/src/components/Schedule.vue
@@ -1,26 +1,13 @@
 <template lang="pug">
   div#scheduleApp
-    .grid-stack.grid-stack-6
-      .grid-stack-item(data-gs-x="0" data-gs-y="0" data-gs-height="2" data-gs-width="1" data-gs-locked)
-        .grid-stack-item-content test
-      .grid-stack-item(data-gs-x="1" data-gs-y="0" data-gs-height="2" data-gs-width="1" data-gs-locked)
-        .grid-stack-item-content bob
-      .grid-stack-item(data-gs-x="2" data-gs-y="0" data-gs-height="2" data-gs-width="1" data-gs-locked)
-        .grid-stack-item-content timmy
-      .grid-stack-item(data-gs-x="3" data-gs-y="0" data-gs-height="2" data-gs-width="1" data-gs-locked)
-        .grid-stack-item-content sam
-      .grid-stack-item(data-gs-x="4" data-gs-y="0" data-gs-height="2" data-gs-width="1" data-gs-locked)
-        .grid-stack-item-content john
-      .grid-stack-item(data-gs-x="5" data-gs-y="0" data-gs-height="2" data-gs-width="1" data-gs-locked)
-        .grid-stack-item-content peter
-      .grid-stack-item(data-gs-x="6" data-gs-y="0" data-gs-height="2" data-gs-width="12" data-gs-locked)
-        .grid-stack-item-content glenn
+    div#schedule
+
 
 
 </template>
 
 <script>
-/* eslint quotes: ["error", "double"] */
+/* eslint-disable*/
 import "../scripts/schedule.js"
 export default {
   name: "Schedule",
