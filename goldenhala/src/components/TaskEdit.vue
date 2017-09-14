d0c9b153b290d3dfed73b850249970e586f479c8
Brandon Yee <brandon.yee150@gmail.com>
2017-09-13 22:16:50 -1000

    TaskEdit Component
    This is the sidebar to edit more details about a task, like in Wunderlist.

---
+++ goldenhala/src/components/TaskEdit.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  p Bob
+</template>
+
+<script>
+/* eslint quotes: ["error", "double"] */
+/* eslint-disable */
+export default {
+  name: "hello",
+  data () {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
