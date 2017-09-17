1a257ee6f75cdf0fbb2c0349f63406e22a4d84a5
Brandon Yee <brandon.yee150@gmail.com>
2017-09-16 23:04:03 -1000

    Added Bulma Customization
    Now time for custom colors, etc.

--- goldenhala/src/components/Planner.vue
+++ goldenhala/src/components/Planner.vue
@@ -120,7 +120,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.ghost > .card {
+.ghost {
   opacity: .5;
   background: red;
 }
--- goldenhala/src/main.js
+++ goldenhala/src/main.js
@@ -2,9 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(Buefy, {
+  defaultIconPack: 'fa'
+})
 
 /* eslint-disable no-new */
 
